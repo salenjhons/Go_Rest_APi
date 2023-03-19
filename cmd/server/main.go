1d1fa684734eb78e3c182d3b82d76ac5a0eeb0a5
salenjhons <electromehanic@rambler.ru>
2023-03-19 21:30:17 +0300

    added migrations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/TutorialEdge/go-rest-api-course/internal/db"
@@ -15,9 +14,16 @@ func Run() error {
 		return err
 	}
 
-	if err := db.Ping(context.Background()); err != nil {
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate db")
 		return err
 	}
+
+	// if err := db.Ping(context.Background()); err != nil {
+	// 	return err
+	// }
+
+	fmt.Println("succesfully connected and pinged db")
 
 	return nil
 }
